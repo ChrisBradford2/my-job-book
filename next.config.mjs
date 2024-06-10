fdc5c7ae91fc61a41467a5f7eb979351dffb99fa
Nicolas Barbarisi <nico.bar2012@gmail.com>
2024-06-10 22:02:04 +0200

    Fix build.

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,11 @@
-import { withPWA } from 'next-pwa';
+import withPWA from 'next-pwa';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-  },
 };
 
-export default withPWA(nextConfig);
+export default withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+})(nextConfig);
